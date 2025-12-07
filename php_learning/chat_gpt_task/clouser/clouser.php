b1065b528279c97c62a247e976c07289c2938477
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2025-12-07 17:07:06 +0000

    php/ change directory

---
+++ php_learning/chat_gpt_task/clouser/clouser.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+function counter(){
+
+$x = 0;
+
+return function() use(&$x){
+
+return $x;
+};
+
+}
+
+$counter1  = counter( );
+
+$counter2  = counter( );
+
+?>
+</body>
+</html>
